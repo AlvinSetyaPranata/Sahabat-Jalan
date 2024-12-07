3a0c67a2e2dcea59e74b28691de7e0c3e908671d
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-12-08 05:33:48 +0700

    UPDATE: MIgrate from sqlite3 to postgresql

--- server/server/server/settings.py
+++ server/server/server/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+from os import getenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +81,19 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': getenv("DB_NAME"),              
+        'USER': getenv("DB_USERNAME"),              
+        'PASSWORD': getenv("DB_PASSWORD"),              
+        'HOST': getenv("DB_HOST"),                       
+        'PORT': getenv("DB_PORT"),                        
     }
 }
+
 
 
 # Password validation
